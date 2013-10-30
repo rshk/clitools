f97c01c100f65e9161090b3e6d1b19fac7942082
Samuele Santi <redshadow@hackzine.org>
2013-10-30 22:14:34 +0100

    Nicer library, more tests

--- clitools/__init__.py
+++ clitools/__init__.py
@@ -23,46 +23,90 @@ Example usage::
 import argparse
 
 
+class Command(object):
+    def __init__(self, func):
+        self.func = func
+        self.parser_args = []
+
+    def __call__(self, *a, **kw):
+        return self.func(*a, **kw)
+
+    def __getattr__(self, name):
+        return getattr(self.func, name)
+
+    @property
+    def command_name(self):
+        name = self.func.__name__
+        if name.startswith('command_'):
+            name = name[len('command_'):]
+        return name
+
+
 class CliApp(object):
     def __init__(self, prog_name='cli-app'):
         self.prog_name = prog_name
         self.parser = argparse.ArgumentParser(prog=prog_name)
         self.subparsers = self.parser.add_subparsers(help='sub-commands')
 
-    def command(self, func=None, args=None):
-        """Decorator used to mark a function as a new sub-command"""
-        def wrapped(func):
-            cmd_name = func.__name__
+    def _wrap_func(self, func):
+        """Make sure the function is wrapped in a Command object"""
+        if not isinstance(func, Command):
+            func = Command(func)
+        return func
 
-            ## Strip the ``command_`` prefix from name
-            if cmd_name.startswith('command_'):
-                cmd_name = cmd_name[len('command_'):]
+    def command(self, func=None, args=None):
+        """
+        Decorator used to register a function as a new sub-command
+        """
+
+        def wrapped(func):
+            func = self._wrap_func(func)
 
             ## Prepare the sub-parser and add arguments
-            subparser = self.subparsers.add_parser(cmd_name)
+            subparser = self.subparsers.add_parser(func.command_name)
             subparser.set_defaults(func=func)
+
             if args is not None:
-                for a, kw in args:
-                    subparser.add_argument(*a, **kw)
-            if hasattr(func, '_parser_args'):
-                ## We reverse it to keep the ordering in the
-                ## decorators..
-                for a, kw in reversed(func._parser_args):
-                    subparser.add_argument(*a, **kw)
+                func.parser_args.extend(args)
+
+            ## Add arguments to subparser
+            for a, kw in reversed(func.parser_args):
+                subparser.add_argument(*a, **kw)
+
+        ## If func is None, we want to return a decorator
         if func is None:
             return wrapped
+
         return wrapped(func)
 
-    def parser_arg(self, *a, **kw):
+    def arg(self, *a, **kw):
         """Decorator to add a parser argument to a given command"""
-        def wrapped(fun):
-            if not hasattr(fun, '_parser_args'):
-                fun._parser_args = []
-            fun._parser_args.append((a, kw))
-            return fun
+
+        def wrapped(func):
+            func = self._wrap_func(func)
+            func.parser_args.append((a, kw))
+            return func
+
         return wrapped
 
-    def run_from_command_line(self, args=None):
+    def flag(self, *a, **kw):
+        """Decorator to add a parser argument to a given command"""
+
+        kw.setdefault('default', False)
+        kw['action'] = 'store_false' if kw['default'] else 'store_true'
+
+        def wrapped(func):
+            func = self._wrap_func(func)
+            func.parser_args.append((a, kw))
+            return func
+
+        return wrapped
+
+    def run(self, args=None):
         """Handle running from the command line"""
         args = self.parser.parse_args(args)
         args.func(args)
+
+    ## for retro-compatibility..
+    parser_arg = arg
+    run_from_command_line = run
--- clitools/tests/test_clitools.py
+++ clitools/tests/test_clitools.py
@@ -2,37 +2,51 @@
 Tests for the CLI tools
 """
 
+from __future__ import print_function
+
+from io import BytesIO
 import subprocess
 import sys
 import textwrap
 
+import pytest
+from mock import patch
 
-def test_simple_script(tmpdir):
-    ## Save the script in a temporary file
-    script = textwrap.dedent("""\
-    from __future__ import print_function
+
+@pytest.fixture
+def sample_script():
     from clitools import CliApp
 
     cli = CliApp()
 
+    ## Used to check simple functionality
     @cli.command
     def hello(args):
         print("Hello, world!")
 
+    ## Used to check removal of the 'command_' prefix
     @cli.command
     def command_example(args):
         print("Example")
 
+    ## Used to test arguments
     @cli.command
     @cli.parser_arg('--name')
-    def hello2(args):
+    def hello_arg_old(args):
         print("Hello, {0}!".format(args.name))
 
+    ## Used to test arguments (new style)
+    @cli.command
+    @cli.arg('--name')
+    def hello_arg(args):
+        print("Hello, {0}!".format(args.name))
+
+    ## Used to test arguments, alternate way
     @cli.command(args=[
         (('--arg1', ), {}),
         (('--arg2', ), {'action': 'append'}),
     ])
-    def example2(args):
+    def hello_arg_ugly(args):
         if args.arg1:
             print(args.arg1)
         if args.arg2:
@@ -41,44 +55,72 @@ def test_simple_script(tmpdir):
     def akw(*a, **kw):
         return (a, kw)
 
+    ## Used to test arguments, alternate way with helper function
     @cli.command(args=[
         akw('--arg1'),
         akw('--arg2', action='append'),
     ])
-    def example3(args):
+    def hello_arg_ugly2(args):
         if args.arg1:
             print(args.arg1)
         if args.arg2:
             print(' '.join(args.arg2))
 
-    if __name__ == '__main__':
-        cli.run_from_command_line()
-    """)
-    dest_file = str(tmpdir.join('myscript.py'))
-    with open(dest_file, 'w') as f:
-        f.write(script)
+    @cli.command
+    @cli.arg('--name')
+    @cli.flag('--bye')
+    def hello_name_new(args):
+        if not args.bye:
+            print("Hello, {0}".format(args.name))
+        else:
+            print("Bye, {0}".format(args.name))
 
-    def check_command(args, output="", returncode=0):
-        proc = subprocess.Popen(
-            [sys.executable, dest_file] + args,
-            stdout=subprocess.PIPE)
-        proc.wait()
-        assert proc.returncode == returncode
-        prog_output = proc.stdout.read()
-        assert prog_output == output
+    @cli.command
+    @cli.arg('--name')
+    @cli.flag('--no-bye', default=True)
+    def hello_name_new_invert(args):
+        if args.no_bye:  # The logic is inverted!!
+            print("Hello, {0}".format(args.name))
+        else:
+            print("Bye, {0}".format(args.name))
 
-    ## Check the program output
-    check_command(['hello'], b"Hello, world!\n")
-    check_command(['hello2', '--name=spam'], b"Hello, spam!\n")
-    check_command(['hello2', '--name', 'egg'], b"Hello, egg!\n")
-    check_command(['example'], b"Example\n")
-    check_command(['example2'], b"")
-    check_command(['example2', '--arg1=Hello'], b"Hello\n")
-    check_command(['example2', '--arg1=Hello', '--arg1=World'],
-                  b"World\n")
-    check_command(['example2', '--arg2=Hello', '--arg2=World'],
-                  b"Hello World\n")
-    check_command(['example2', '--arg1=AAAA', '--arg2=Hello', '--arg2=World'],
-                  b"AAAA\nHello World\n")
-    check_command(['example3', '--arg1=AAAA', '--arg2=Hello', '--arg2=World'],
-                  b"AAAA\nHello World\n")
+    return cli
+
+
+def test_simple_script_internal(sample_script):
+    pairs = [
+        (['hello'], b"Hello, world!\n"),
+
+        (['example'], b"Example\n"),
+
+        (['hello_arg_old', '--name=spam'], b"Hello, spam!\n"),
+        (['hello_arg_old', '--name', 'egg'], b"Hello, egg!\n"),
+
+        (['hello_arg', '--name=spam'], b"Hello, spam!\n"),
+        (['hello_arg', '--name', 'egg'], b"Hello, egg!\n"),
+
+        (['hello_arg_ugly', '--arg1=A1'], b"A1\n"),
+        (['hello_arg_ugly', '--arg1=A1', '--arg2=B1'], b"A1\nB1\n"),
+        (['hello_arg_ugly', '--arg1=A1', '--arg1=A2',
+          '--arg2=B1', '--arg2=B2'],
+         b"A2\nB1 B2\n"),
+
+        (['hello_arg_ugly2', '--arg1=A1'], b"A1\n"),
+        (['hello_arg_ugly2', '--arg1=A1', '--arg2=B1'], b"A1\nB1\n"),
+        (['hello_arg_ugly2', '--arg1=A1', '--arg1=A2',
+          '--arg2=B1', '--arg2=B2'],
+         b"A2\nB1 B2\n"),
+
+        (['hello_name_new', '--name=world'], b'Hello, world\n'),
+        (['hello_name_new', '--name=world', '--bye'], b'Bye, world\n'),
+
+        (['hello_name_new_invert', '--name=world'], b'Hello, world\n'),
+        (['hello_name_new_invert', '--name=world', '--no-bye'],
+         b'Bye, world\n'),
+    ]
+
+    for args, expected in pairs:
+        dummy_output = BytesIO()
+        with patch('sys.stdout', dummy_output):
+            sample_script.run(args)
+            assert dummy_output.getvalue() == expected
